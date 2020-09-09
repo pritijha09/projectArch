f9bc23387be970464572018cd2793d3295e93871
priti <priti.kumari@primussoft.com>
2020-09-09 17:13:30 +0530

    add landing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardComponent } from './landing-component/dashboard/dashboard.component';
+import { LandingComponent } from './landing-component/landing/landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
